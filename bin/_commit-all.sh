a26ad6e1f4565e7e7a25266b1dbfd78a471d9a3c
jdlxt <jdlxt@localhost>
2021-05-20 10:50:51 +0200

    auto sync

--- bin/_commit-all.sh
+++ bin/_commit-all.sh
@@ -1,35 +1,46 @@
-cd ..
+cd ../woof-package/woof
 
 echo "Commiting woof-theme"
-cd  woof-package/woof-theme
+cd  woof-theme
 git add . && git commit -m "auto sync" && git push
-
+cd ..
+echo "------------------------------"
 
 echo "Commiting woof-model"
-cd ../woof-model
+cd woof-model
 git add . && git commit -m "auto sync" && git push
+cd ..
+echo "------------------------------"
 
 
 echo "Commiting woof-view"
-cd ../woof-view
+cd woof-view
 git add . && git commit -m "auto sync" && git push
+cd ..
+echo "------------------------------"
+
+cd ../../bin
 
 # ======================================================================
-cd ../..
+cd ../woof-package/phi
 
 echo "Commiting phi-filesystem"
-cd ../phi-filesystem
+cd phi-filesystem
 git add . && git commit -m "auto sync" && git push
+cd ..
+echo "------------------------------"
 
 echo "Commiting phi-traits"
-cd ../phi-traits
+cd phi-traits
 git add . && git commit -m "auto sync" && git push
+cd ..
+echo "------------------------------"
 
-
+cd ../../bin
 
 # ======================================================================
 echo "Commiting woof"
-cd ../..
+
 git add . && git commit -m "auto sync" && git push
 
 
